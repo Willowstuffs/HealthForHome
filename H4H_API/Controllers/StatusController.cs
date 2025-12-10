b4b3a24198a29b1e892e9b90ba4103514fb61e95
Wiktor R <Wiktorus565@gmail.com>
2025-12-10 20:55:17 +0100

    Konfiguracja Gita u mnie (wiktorrem) i pierwszy draft projektu z strukturą plików i kontrolerem do sprawdzania stanu serwera StatusController

---
+++ H4H_API/Controllers/StatusController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace H4H_API.Controllers
+{
+    [ApiController] //wlacza automatyczna walidacje modelu
+    [Route("api/[controller]")] //sciezka api/status
+    public class StatusController : ControllerBase
+    {
+        [HttpGet] //metoda GET
+        public IActionResult GetStatus()
+        { //ok200 z timestampem
+            return Ok(new { status = "API is running", time = DateTime.Now });
+        }
+    }
+}
